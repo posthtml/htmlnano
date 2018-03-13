50583975c097ab2e27df80c65e7fc1349d6c827e
Kirill Maltsev <maltsevkirill@gmail.com>
2018-03-13 20:14:57 +0200

    Test SVG with <use> minification #47

--- test/modules/minifySvg.js
+++ test/modules/minifySvg.js
@@ -10,10 +10,12 @@ describe('minifySvg', () => {
 
                 <circle cx="150" cy="100" r="80" fill="green" />
 
-                <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+                <text id="myText" x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+
+                <use href="#myText" x="2" y="2"></use>
             </svg>`,
 
-            '<svg baseProfile="full" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect width="100%" height="100%" fill="red"/><circle cx="150" cy="100" r="80" fill="green"/><text x="150" y="125" font-size="60" text-anchor="middle" fill="#fff">SVG</text></svg>',
+            '<svg baseProfile="full" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect width="100%" height="100%" fill="red"/><circle cx="150" cy="100" r="80" fill="green"/><text id="a" x="150" y="125" font-size="60" text-anchor="middle" fill="#fff">SVG</text><use href="#a" x="2" y="2"/></svg>',
 
             options
         );
