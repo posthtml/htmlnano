d2eb67e92edc89fb578f2fcce32fd3499f7a15de
Kirill Maltsev <maltsevkirill@gmail.com>
2018-08-03 14:38:45 +0300

    Test disabling SVGO optimizations
    In #53 I has been asked about disabling SVGO optimizations.
    So I think it'll be nice to add a related test to check that.

--- test/modules/minifySvg.js
+++ test/modules/minifySvg.js
@@ -2,22 +2,39 @@ import { init } from '../htmlnano';
 
 describe('minifySvg', () => {
     const options = {minifySvg: {}};
+    const svg = `<svg version="1.1" baseProfile="full" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <rect width="100%" height="100%" fill="red" />
+
+        <circle cx="150" cy="100" r="80" fill="green" />
+
+        <text id="myText" x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+
+        <use href="#myText" x="2" y="2"></use>
+    </svg>`;
 
     it('should minify SVG inside <svg>', () => {
         return init(
-            `<svg version="1.1" baseProfile="full" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-                <rect width="100%" height="100%" fill="red" />
-
-                <circle cx="150" cy="100" r="80" fill="green" />
-
-                <text id="myText" x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
-
-                <use href="#myText" x="2" y="2"></use>
-            </svg>`,
+            svg,
 
             '<svg baseProfile="full" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect width="100%" height="100%" fill="red"/><circle cx="150" cy="100" r="80" fill="green"/><text id="a" x="150" y="125" font-size="60" text-anchor="middle" fill="#fff">SVG</text><use href="#a" x="2" y="2"/></svg>',
 
             options
+        );
+    });
+
+    it('shouldn\'t minify SVG colors if this option is disabled', () => {
+        return init(
+            svg,
+
+            '<svg baseProfile="full" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect width="100%" height="100%" fill="red"/><circle cx="150" cy="100" r="80" fill="green"/><text id="a" x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text><use href="#a" x="2" y="2"/></svg>',
+
+            {
+                minifySvg: {
+                    plugins: [
+                        { convertColors: false }
+                    ]
+                }
+            }
         );
     });
 });
