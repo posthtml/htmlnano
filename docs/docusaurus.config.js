a92c5bdbc8e036bdf608785bc2203f1ecb17830a
Kirill Maltsev <maltsevkirill@gmail.com>
2021-08-21 12:43:58 +0300

    Use 4 space for indention in docs/
    To have it consistent with the main code

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -3,47 +3,46 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'htmlnano',
-  tagline: 'Modular HTML minifier',
-  url: 'https://htmlnano.netlify.app',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  organizationName: 'posthtml',  
-  projectName: 'htmlnano', 
-  trailingSlash: false,
-  themeConfig: {
-    navbar: {
-      title: 'htmlnano',
-      items: [
-        {
-          type: 'docsVersionDropdown',
-          position: 'right',
-          dropdownActiveClassDisabled: true,
+    title: 'htmlnano',
+    tagline: 'Modular HTML minifier',
+    url: 'https://htmlnano.netlify.app',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    organizationName: 'posthtml',
+    projectName: 'htmlnano',
+    trailingSlash: false,
+    themeConfig: {
+        navbar: {
+            title: 'htmlnano',
+            items: [
+                {
+                    type: 'docsVersionDropdown',
+                    position: 'right',
+                    dropdownActiveClassDisabled: true,
+                },
+                {
+                    href: 'https://github.com/posthtml/htmlnano',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
         },
-        {
-          href: 'https://github.com/posthtml/htmlnano',
-          label: 'GitHub',
-          position: 'right',
+        prism: {
+            theme: lightCodeTheme,
+            darkTheme: darkCodeTheme,
         },
-      ],
     },
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '/', 
-          editUrl:
-            'https://github.com/posthtml/htmlnano/edit/master/docs/',
-        },
-      },
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            {
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    routeBasePath: '/',
+                    editUrl: 'https://github.com/posthtml/htmlnano/edit/master/docs/',
+                },
+            },
+        ],
     ],
-  ],
 };
