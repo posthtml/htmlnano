ab5a39dfae8e0bc025466c5831f4c327256ea3fc
Eric Liu <ericyl.us@gmail.com>
2022-07-28 20:07:47 -0700

    Add TypeScript definitions
    Closes #201

---
+++ index.d.ts
@@ -0,0 +1,92 @@
+import type PostHTML from "posthtml";
+
+interface HtmlnanoOptions {
+  skipConfigLoading?: boolean;
+  collapseAttributeWhitespace?: boolean;
+  collapseBooleanAttributes?: {
+    amphtml?: boolean;
+  };
+  collapseWhitespace?: "conservative" | "all" | "aggressive";
+  custom?: (tree: PostHTML.Node, options?: any) => PostHTML.Node;
+  deduplicateAttributeValues?: boolean;
+  minifyUrls?: boolean;
+  mergeStyles?: boolean;
+  mergeScripts?: boolean;
+  minifyCss?: {
+    preset?: any;
+  };
+  minifyConditionalComments?: boolean;
+  minifyJs?: any;
+  minifyJson?: any;
+  minifySvg?: any;
+  normalizeAttributeValues?: boolean;
+  removeAttributeQuotes?: boolean;
+  removeComments?: boolean | "safe" | "all" | RegExp | (() => void);
+  removeEmptyAttributes?: boolean;
+  removeRedundantAttributes?: boolean;
+  removeOptionalTags?: boolean;
+  removeUnusedCss?: boolean;
+  sortAttributes?: boolean;
+  sortAttributesWithLists?: "alphabetical" | "frequency";
+}
+
+interface HtmlnanoPreset extends Omit<HtmlnanoOptions, "skipConfigLoading"> {}
+
+interface Presets {
+  safe: HtmlnanoPreset;
+  ampSafe: HtmlnanoPreset;
+  max: HtmlnanoPreset;
+}
+
+type Preset = Presets["safe" | "ampSafe" | "max"];
+
+export function loadConfig(
+  options?: HtmlnanoOptions,
+  preset?: Preset,
+  configPath?: string
+): [HtmlnanoOptions | {}, Preset];
+
+declare function htmlnano<TMessage>(
+  options?: HtmlnanoOptions,
+  preset?: Preset
+): (tree: PostHTML.Node) => Promise<PostHTML.Result<TMessage>>;
+
+interface PostHtmlOptions {
+  directives?: Array<{
+    name: string | RegExp;
+    start: string;
+    end: string;
+  }>;
+  sourceLocations?: boolean;
+  recognizeNoValueAttribute?: boolean;
+  xmlMode?: boolean;
+  decodeEntities?: boolean;
+  lowerCaseTags?: boolean;
+  lowerCaseAttributeNames?: boolean;
+  recognizeCDATA?: boolean;
+  recognizeSelfClosing?: boolean;
+  Tokenizer?: any;
+}
+
+declare namespace htmlnano {
+  export function process<TMessage>(
+    html: string,
+    options?: HtmlnanoOptions,
+    preset?: Preset,
+    postHtmlOptions?: PostHtmlOptions
+  ): Promise<PostHTML.Result<TMessage>>;
+
+  export function getRequiredOptionalDependencies(
+    optionsRun?: HtmlnanoOptions,
+    presetRun?: Preset
+  ): string[];
+
+  export function htmlMinimizerWebpackPluginMinify(
+    input: { [file: string]: string },
+    minimizerOptions?: HtmlnanoOptions
+  ): any;
+
+  export const presets: Presets;
+}
+
+export default htmlnano;
