6462a67e9fc32fee748ef1879f4b0a686af9cc5d
Kirill Maltsev <maltsevkirill@gmail.com>
2025-04-30 10:40:06 +0300

    [tmp] replace .eslintrc with eslint.config.js and update lint script and dependencies

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginPathImportExtension from 'eslint-plugin-path-import-extension';
+
+export default [
+    {
+        files: ['*.mjs', 'lib/**/*.mjs', 'test/**/*.mjs'],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            parser: require('@babel/eslint-parser'),
+            parserOptions: {
+                requireConfigFile: false
+            },
+            globals: {
+                node: true,
+                browser: false,
+                mocha: true
+            }
+        },
+        plugins: {
+            'import': eslintPluginImport,
+            'path-import-extension': eslintPluginPathImportExtension
+        },
+        rules: {
+            'indent': [2, 4, {"SwitchCase": 1}],
+            'quotes': [2, "single"],
+            'linebreak-style': [2, "unix"],
+            'semi': [2, "always"],
+            'camelcase': [2, {"properties": "always"}],
+            'brace-style': [2, "1tbs", {"allowSingleLine": true}],
+            'import/no-commonjs': "error",
+            'import/no-dynamic-require': "error",
+            'import/extensions': ["error", "always", { "mjs": "never" }],
+            'path-import-extension/path-import-extension': ["error", { "extensions": [".mjs"] }]
+        },
+        settings: {
+            'import/extensions': ['.js', '.cjs', '.mjs']
+        }
+    }
+];
