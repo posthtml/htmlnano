f03ef4152a86b20fd90084b6df6144da7b0ac8d7
Kirill Maltsev <maltsevkirill@gmail.com>
2025-04-30 10:45:39 +0300

    [tmp] rename eslint.config.js to eslint.config.mjs and update lint script in package.json accordingly

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import eslintPluginImport from 'eslint-plugin-import';
-import * as eslintPluginPathImportExtension from 'eslint-plugin-path-import-extension';
+// import pathImportExtension from 'eslint-plugin-path-import-extension';
 import babelEslintParser from '@babel/eslint-parser';
 
 export default [
@@ -25,19 +25,19 @@ export default [
         },
         plugins: {
             'import': eslintPluginImport,
-            'path-import-extension': eslintPluginPathImportExtension
+            // 'path-import-extension': pathImportExtension
         },
         rules: {
-            'indent': [2, 4, {"SwitchCase": 1}],
-            'quotes': [2, "single"],
-            'linebreak-style': [2, "unix"],
-            'semi': [2, "always"],
-            'camelcase': [2, {"properties": "always"}],
-            'brace-style': [2, "1tbs", {"allowSingleLine": true}],
-            'import/no-commonjs': "error",
-            'import/no-dynamic-require': "error",
-            'import/extensions': ["error", "always", { "mjs": "never" }],
-            'path-import-extension/path-import-extension': ["error", { "extensions": [".mjs"] }]
+            'indent': [2, 4, {'SwitchCase': 1}],
+            'quotes': [2, 'single'],
+            'linebreak-style': [2, 'unix'],
+            'semi': [2, 'always'],
+            'camelcase': [2, {'properties': 'always'}],
+            'brace-style': [2, '1tbs', {'allowSingleLine': true}],
+            'import/no-commonjs': 'error',
+            'import/no-dynamic-require': 'error',
+            'import/extensions': ['error', 'always', { 'mjs': 'never' }],
+            // 'path-import-extension/path-import-extension': ["error", { "extensions": [".mjs"] }]
         },
         settings: {
             'import/extensions': ['.js', '.cjs', '.mjs']
